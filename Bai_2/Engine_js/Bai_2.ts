6ceea957e06e92658ba9455a9e658bdc0d4a3ff7
codevcn <codevoicainay@gmail.com>
2022-09-01 13:17:52 +0700

    share file to Tuan

---
+++ Bai_2/Engine_js/Bai_2.ts
@@ -0,0 +1,4 @@
+import find_parentEle_hasClass from 'C:/Users/dell/Documents/Code_VCN/Lib/functions'
+import {alignCenter_absolute_cross} from 'C:/Users/dell/Documents/Code_VCN/Lib/functions'
+
+console.log('oke')
